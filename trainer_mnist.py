d9113ee18f56820d58995fc65fc58184f44d9c93
mizunt1 <mizunt@gmail.com>
2021-11-22 00:07:14 -0500

    removed sacred

--- trainer_mnist.py
+++ trainer_mnist.py
@@ -1,32 +1,12 @@
-# run as python trainer_minist.py without sacred logging
-# run as python trainer_mnist.py -m <sacred_file_name> with sacred logging
-import os
-
 import torch
 import torch.optim as optim
 import torchvision
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from sacred import Experiment
-from sacred.observers import MongoObserver
-from sacred.observers import FileStorageObserver
-from neptunecontrib.monitoring.sacred import NeptuneObserver
-import neptune.new as neptune
 
 from models.mnist import BayesianMnist
 
-#token = os.environ.get("NEPTUNE_API_TOKEN")
-#nep_run = neptune.init(api_token=token, project='bbb')
-
-
-
-ex = Experiment()
-ex.observers.append(FileStorageObserver)
-#ex.observers.append(NeptuneObserver("bbb"))
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("using")
-print(device)
 train_data = torchvision.datasets.MNIST(root="~/data", train=True,
                                         download=True,
                                         transform=transforms.Compose([
@@ -39,14 +19,6 @@ test_data = torchvision.datasets.MNIST(root="~/data", train=False,
                                            transforms.ToTensor()]
                                        ))
 
-@ex.config
-def config():
-    num_epochs = 10
-    batch_size = 28
-    elbo_samples = 4
-    cuda = False
-
-@ex.automain
 def train(elbo_samples, batch_size, num_epochs, cuda):
     device = torch.device("cuda" if cuda else "cpu")
     model = BayesianMnist(28*28).to(device)
@@ -73,8 +45,6 @@ def train(elbo_samples, batch_size, num_epochs, cuda):
             if batch_number % 400 == 0:
                 print("batch number: {} batch TRAIN loss: {} ".format(batch_number, loss.item()))
                 print("batch number: {} batch TRAIN accuracy: {} ".format(batch_number, accuracy))
-                ex.log_scalar("batch train loss", loss.item())
-                ex.log_scalar("batch train accuracy", accuracy)
                 total_test_loss = 0
                 for x_test, y_test in loader_test:
                     x_test = x_test.reshape(batch_size, -1).to(device)
@@ -86,10 +56,8 @@ def train(elbo_samples, batch_size, num_epochs, cuda):
                     "batch number {} batch TEST loss: {} ".format(batch_number, batch_test_loss))
                 print(
                     "batch number {} batch TEST acc: {} ".format(batch_number, batch_test_accuracy))
-                ex.log_scalar("batch test loss", loss.item())
-                ex.log_scalar("batch test accuracy", accuracy)
 
         print("total loss for epoch {} : {} ".format(epoch, epoch_loss))
 
 
-train(num_epochs=10, batch_size=28,  elbo_samples=3, cuda=True)
+train(num_epochs=10, batch_size=28,  elbo_samples=3, cuda=False)
